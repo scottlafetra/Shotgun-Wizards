40b562a2850fc501a8057a6c6307dd710ae47011
Scott La Fetra <scottlafetra@protonmail.com>
2016-03-11 15:52:04 -0800

    Implemented Nature Wizard's spells

---
+++ Assets/Scripts/Projectiles/HealTargeting.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealTargeting : MonoBehaviour {
+
+    public float hps = 1;
+
+    private Targeter myTargeter;
+
+	// Use this for initialization
+	void Start () {
+        myTargeter = GetComponent<Targeter>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    foreach(GameObject target in myTargeter.GetTargets()) {
+            StatsHandler targetStats = target.GetComponent<StatsHandler>();
+
+            if(targetStats != null) {
+                targetStats.ChangeHealth(hps * Time.deltaTime);
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/Projectiles/SlowsWhileTouching.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SlowsWhileTouching : MonoBehaviour {
+
+    public float slowAmount;
+
+    private Targeter myTargeter;
+
+    void Start() {
+        myTargeter = GetComponent<Targeter>();
+    }
+
+	void OnTriggerEnter(Collider other) {
+        StatsHandler otherStats = other.GetComponent<StatsHandler>();
+
+        if (otherStats != null) {
+
+            otherStats.moveSpeed -= slowAmount;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        StatsHandler otherStats = other.gameObject.GetComponent<StatsHandler>();
+
+        if (otherStats != null) {
+
+            otherStats.moveSpeed += slowAmount;
+        }
+    }
+
+    void OnDisable() {
+        List<GameObject> targets = myTargeter.GetTargets();
+        
+        foreach (GameObject target in targets) {
+            StatsHandler targetStats = target.GetComponent<StatsHandler>();
+
+            if (targetStats != null) {
+
+                targetStats.moveSpeed += slowAmount;
+            }
+        }
+    }
+}
